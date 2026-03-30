208803c46839163d92b0b40290cd42c245d2fb41
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2026-03-30 22:25:38 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        List<List<String>> result = new ArrayList<>();
+        boolean[] visited = new boolean[strs.length];
+
+        for (int i = 0; i < strs.length; i++) {
+            if (visited[i]) continue;
+            List<String> group = new ArrayList<>();
+
+            for (int j = i; j < strs.length; j++) {
+
+                if (visited[j]) continue;
+
+                char[] a = strs[i].toCharArray();
+                char[] b = strs[j].toCharArray();
+
+                Arrays.sort(a);
+                Arrays.sort(b);
+
+                if (Arrays.equals(a, b)) {
+                    group.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+            visited[i] = true;
+            result.add(group);
+        }
+
+        return result;
+    }
+}
